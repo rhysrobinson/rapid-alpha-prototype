385375f61aad54f99a43c5af8f17cde7710fc29c
Rhys Robinson <Rhys2.Robinson@live.uwe.ac.uk>
2022-02-14 11:48:38 +0000

    Update tilemap

--- tilemap.gd
+++ tilemap.gd
@@ -1,10 +1,22 @@
 extends TileMap
 
 func _input(event: InputEvent) -> void:
+	var tile = world_to_map(get_global_mouse_position())
+	var cell = get_cell(tile.x, tile.y)
+
 	if event.is_action_pressed("remove"):
-		var tile = world_to_map(get_global_mouse_position())
-		set_cell(tile.x, tile.y, -1)
+		if cell == 5:
+			pass
+		elif cell == 6:
+			set_cell(tile.x, tile.y, 5)
+		elif cell == 7:
+			set_cell(tile.x, tile.y, 6)
+
 	elif event.is_action_pressed("place"):
-		var tile = world_to_map(get_global_mouse_position())
-		set_cell(tile.x, tile.y, 6)
+		if cell == 5:
+			set_cell(tile.x, tile.y, 6)
+		elif cell == 6:
+			set_cell(tile.x, tile.y, 7)
+		else:
+			pass
 	pass
